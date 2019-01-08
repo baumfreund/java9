d937a0ef35b173c25287a8979053e9e230249a9b
baumfreund22 <Tom@Tom-Aspire>
2019-01-08 16:16:50 +0100

    initial commit

---
+++ java9/src/eu/selfhost/baumfreund22/java9/ProcessInformation.java
@@ -0,0 +1,17 @@
+package eu.selfhost.baumfreund22.java9;
+
+import java.util.stream.Collectors;
+
+public class ProcessInformation {
+
+	public static void main(String[] args) {
+		
+//		Einfacherer Zugriff auf Prozessinformationen
+		ProcessHandle.current().pid();
+		ProcessHandle.allProcesses().forEach(e -> System.out.println("Prozess: "+e.pid()+", Info: "+e.info().toString()));
+		
+		
+		
+	}
+
+}
