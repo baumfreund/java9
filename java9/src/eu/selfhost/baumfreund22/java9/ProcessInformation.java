5ea42d4b8b19976902be9d39fd8b589bfa24ff46
baumfreund22 <Tom@Tom-Aspire>
2019-01-08 16:39:47 +0100

    Detailed process information

--- java9/src/eu/selfhost/baumfreund22/java9/ProcessInformation.java
+++ java9/src/eu/selfhost/baumfreund22/java9/ProcessInformation.java
@@ -7,11 +7,14 @@ public class ProcessInformation {
 	public static void main(String[] args) {
 		
 //		Einfacherer Zugriff auf Prozessinformationen
-		ProcessHandle.current().pid();
+
 		ProcessHandle.allProcesses().forEach(e -> System.out.println("Prozess: "+e.pid()+", Info: "+e.info().toString()));
 		
-//		Test
-//		Branch 2
+		final ProcessHandle current = ProcessHandle.current();
+        System.out.println("PID:       " + current.pid());
+        System.out.println("Info:      " + current.info());
+        System.out.println("Command:   " + current.info().command());
+        System.out.println("CPU-Usage: " + current.info().totalCpuDuration());
 		
 	}
 
